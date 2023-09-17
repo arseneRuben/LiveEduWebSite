17927ee327c555e89fd73b2c5fb42ca3d7d66264
Brandon Kamga <tamwabrandon8@gmail.com>
2023-09-17 07:35:23 +0200

    translation test Api

--- translate.js
+++ translate.js
@@ -1,17 +1,21 @@
 function googleTranslateElementInit() {
     new google.translate.TranslateElement({
         pageLanguage: 'en',  // Langue d'origine (français)
-        includedLanguages: 'fr,es',  // Langues de destination (anglais, espagnol)
+        includedLanguages: 'fr,es,en,de,am,sw,ar,ha,ig,so,mg,sn,sw,tn,yo',  // Langues de destination (anglais, espagnol, allemand, amharique, swahili, arabe, haoussa, igbo, somali, malgache, shona, suédois, tswana, yoruba)
         autoDisplay: false  // Désactiver l'affichage automatique du widget de traduction
     }, 'google_translate_element');
 }
 
 document.getElementById('translateButton').addEventListener('click', function () {
     // Afficher le widget de traduction
-    new google.translate.TranslateElement({
+    const translateWidget = new google.translate.TranslateElement({
         pageLanguage: 'en',         // Langue d'origine (anglais)
-        includedLanguages: 'fr,es', // Langues de destination (français, espagnol)
+        includedLanguages: 'en,fr,es,de,am,sw,ar,ha,ig,so,mg,sn,sw,tn,yo', // Langues de destination (français, espagnol, allemand, amharique, swahili, arabe, haoussa, igbo, somali, malgache, shona, suédois, tswana, yoruba)
         autoDisplay: false           // Activer l'affichage automatique du widget de traduction
     }, 'google_translate_element');
-});
 
+    // Personnalisation du contenu du widget
+    translateWidget.showBanner(false); // Cacher la bannière du widget
+    translateWidget.showPoweredBy(false); // Cacher le texte "Propulsé par Google Translate"
+    translateWidget.disableAutoTranslation(); // Désactiver la traduction automatique
+});
