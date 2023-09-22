c6c9d4fa1a2fd872a61208a83559b884825ea2bf
Arsene Fokam P <2210100@isi-montreal.com>
2023-09-22 18:31:41 -0400

    video

--- translate.js
+++ translate.js
@@ -1,21 +1,22 @@
 function googleTranslateElementInit() {
     new google.translate.TranslateElement({
         pageLanguage: 'en',  // Langue d'origine (français)
-        includedLanguages: 'fr,es,en,de,am,sw,ar,ha,ig,so,mg,sn,sw,tn,yo',  // Langues de destination (anglais, espagnol, allemand, amharique, swahili, arabe, haoussa, igbo, somali, malgache, shona, suédois, tswana, yoruba)
-        autoDisplay: false  // Désactiver l'affichage automatique du widget de traduction
+        includedLanguages: 'fr,es,en,de,am,ig,so,mg,sn,sw',  // Langues de destination (anglais, espagnol, allemand, amharique, swahili, arabe, haoussa, igbo, somali, malgache, shona, suédois, tswana, yoruba)
+        autoDisplay: true  // Désactiver l'affichage automatique du widget de traduction
     }, 'google_translate_element');
 }
 
-document.getElementById('translateButton').addEventListener('click', function () {
+document.getElementById('translateButton').addEventListener('click', function (e) {
     // Afficher le widget de traduction
+    e.preventDefault();
     const translateWidget = new google.translate.TranslateElement({
         pageLanguage: 'en',         // Langue d'origine (anglais)
-        includedLanguages: 'en,fr,es,de,am,sw,ar,ha,ig,so,mg,sn,sw,tn,yo', // Langues de destination (français, espagnol, allemand, amharique, swahili, arabe, haoussa, igbo, somali, malgache, shona, suédois, tswana, yoruba)
-        autoDisplay: false           // Activer l'affichage automatique du widget de traduction
+        includedLanguages: 'en,fr,es,de,am,ig,so,mg,sn,sw', // Langues de destination (français, espagnol, allemand, amharique, swahili, arabe, haoussa, igbo, somali, malgache, shona, suédois, tswana, yoruba)
+        autoDisplay: true           // Activer l'affichage automatique du widget de traduction
     }, 'google_translate_element');
 
     // Personnalisation du contenu du widget
-    translateWidget.showBanner(false); // Cacher la bannière du widget
-    translateWidget.showPoweredBy(false); // Cacher le texte "Propulsé par Google Translate"
+    translateWidget.showBanner(true); // Cacher la bannière du widget
+    translateWidget.showPoweredBy(true); // Cacher le texte "Propulsé par Google Translate"
     translateWidget.disableAutoTranslation(); // Désactiver la traduction automatique
 });
